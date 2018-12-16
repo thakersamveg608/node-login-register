19798fab3d1d1c56731df089d7a9155ab63ecfb4
Samveg <thakersamveg608@gmail.com>
2018-12-16 21:40:17 +0530

    basic login register working

--- routes/login.js
+++ routes/login.js
@@ -1,28 +1,38 @@
 var myFunctions = require('../app');
 var express = require('express');
+var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 
 var User = mongoose.model('User');
+var jsonParser = bodyParser.json();
 
-router.post('/login', (req, res, next) => {
-    User.find({'username' : req.param('username')}, 'password', (err, user) => {
+router.post('/', jsonParser, (req, res, next) => {
+    // console.log(req.body);
+    console.log(req);
+    User.find({'username' : req.body.username}, 'password', (err, user) => {
         if(err)
             console.log("Error in accessing DB");
         else{
-            if(user.password === req.param('password')){
-                res.send(JSON.stringify({
-                    success : true,
-                    message : "User successfully logged in"
-                }))
-            }
-            else{
-                res.send(JSON.stringify({
-                    success : false,
-                    message : "Invalid password"
-                }))
-            }
+            console.log(user);
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                console.log(result);
+                if(result){
+                    console.log("true hai");
+                    res.send(JSON.stringify({
+                        success : true,
+                        message : "User successfully logged in"
+                    }));
+                }
+                else{
+                    console.log("false hai");
+                    res.send(JSON.stringify({
+                        success : false,
+                        message : "Invalid password"
+                    }));
+                }
+            })
         }
     });
 });
--- routes/register.js
+++ routes/register.js
@@ -1,32 +1,42 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 var User = require('../models/user');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-var encryptedPass = '';
 
-router.post('/register', (req, res, next) => {
-  bcrypt.hash(req.param('password'), saltRounds, (err, pass) => {
+var jsonParser = bodyParser.json();
+
+router.post('/', jsonParser ,(req, res, next) => {
+
+  // console.log(req);
+  // console.log(req.body);
+
+  bcrypt.hash(req.body.password, saltRounds, (err, pass) => {
     if(err)
       console.log("Error in hashing password");
     else
-      encryptedPass = pass;
-  });
-  var newUser = new User({
-    username : req.param('username'),
-    email : req.param('email'),
-    mobile : req.param('contact'),
-    password : encryptedPass
-  });
-  newUser.save(function(err){
-    console.log("User Saved");
-  });
-
-  res.send(JSON.stringify({
-    success : true,
-    message : 'Registeration successful'
-  }));
-
+     { 
+        console.log("REACHED HERE");
+        var encryptedPass = pass;
+        console.log("ENCRYPTTED" + encryptedPass);
+        var newUser = new User({
+          username : req.body.username,
+          email : req.body.email,
+          mobile : req.body.contact,
+          password : encryptedPass
+        });
+        console.log(newUser.password);
+        newUser.save(function(err){
+          console.log("User Saved");
+        });
+      
+        res.send(JSON.stringify({
+          success : true,
+          message : 'Registeration successful'
+        }));
+      }  
+});
 });
 
 module.exports = router;
